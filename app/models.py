d9a226c2ad724bc5101a3e4a9b6eea1171ab3438
Saveliy19 <saveliy200319@gmail.com>
2024-05-03 14:22:22 +0300

    Added gettiing information about user and petition

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr
+from typing import List
 
 class UserToLogin(BaseModel):
     email: EmailStr
@@ -6,6 +7,29 @@ class UserToLogin(BaseModel):
 
 class Token(BaseModel):
     token: str
+
+class UserID(BaseModel):
+    id: int
+
+# класс с краткой информации о петиции
+class PetitionWithHeader(BaseModel):
+    id: int
+    header: str
+
+class PetitionsCity(BaseModel):
+    petitions: List[PetitionWithHeader]
+
+class UserToFrontend(BaseModel):
+    id: int
+    last_name: str
+    first_name: str
+    patronymic: str
+    rating: float
+    city: str
+    region: str
+    petitions: List[PetitionWithHeader]
+
+
 
 class UserToRegistrate(BaseModel):
     email: EmailStr
@@ -16,3 +40,26 @@ class UserToRegistrate(BaseModel):
     rating: float
     is_moderator: bool
     city: int
+
+class City(BaseModel):
+    region: str
+    name: str
+
+class PetitonID(BaseModel):
+    id: int
+
+# класс для возврата полной информации по заявке
+class PetitionData(BaseModel):
+    id: int
+    header: str
+    is_initiative: bool
+    category: str
+    description: str
+    status: str
+    petitioner_id: int
+    submission_time: str
+    latitude: float
+    longitude: float
+    likes_count: int
+    region: str
+    city_name: str
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException, status
 import aiohttp
+import asyncio
 
-from app.models import UserToLogin, Token, UserToRegistrate
+from app.models import UserToLogin, Token, UserToRegistrate, UserID, UserToFrontend, PetitionsCity, City, PetitonID, PetitionData
 from app.utils import send_to_get_data
-from app.config import CLIENT_SERVICE_ADDRESS
+from app.config import CLIENT_SERVICE_ADDRESS, PETITION_SERVICE_ADDRESS
 
 router = APIRouter()
 
@@ -14,7 +15,7 @@ async def registrate_new_user(user: UserToRegistrate):
         result = await send_to_get_data(CLIENT_SERVICE_ADDRESS + '/registration', user)
         return result
     except aiohttp.ClientError as e:
-        raise HTTPException(status_code=500, detail="Error communicating with the destination service")
+        raise HTTPException(status_code=500, detail=str(e))
 
 #  маршрут для авторизации и получения jwt токена
 @router.post("/login")
@@ -23,13 +24,59 @@ async def login(user: UserToLogin):
         result = await send_to_get_data(CLIENT_SERVICE_ADDRESS + '/token', user)
         return result
     except aiohttp.ClientError as e:
-        raise HTTPException(status_code=500, detail="Error communicating with the destination service")
+        raise HTTPException(status_code=500, detail=str(e))
     
 # маршрут личного кабинета пользователя
 @router.post("/me")
 async def get_information_about_user(token: Token):
     try:
-        result = await send_to_get_data(CLIENT_SERVICE_ADDRESS + '/get_data', token)
-        return result
+        user_info = await send_to_get_data(CLIENT_SERVICE_ADDRESS + '/get_data', token)
     except aiohttp.ClientError as e:
-        raise HTTPException(status_code=500, detail="Error communicating with the destination service")
+        raise HTTPException(status_code=500, detail=str(e))
+    try:
+        user_petitions = await send_to_get_data(PETITION_SERVICE_ADDRESS + '/get_petitions', UserID(id=user_info[0]["id"]))
+        user_petitions_list = user_petitions[0]["petitions"]
+    except aiohttp.ClientError:
+        # Если сервис петиций не отвечает, то список заявок пользователя пуст
+        user_petitions_list = []
+    return UserToFrontend(id = user_info[0]["id"],
+                          last_name = user_info[0]["last_name"],
+                          first_name = user_info[0]["first_name"],
+                          patronymic = user_info[0]["patronymic"],
+                          rating = user_info[0]["rating"],
+                          city = user_info[0]["city"],
+                          region = user_info[0]["region"],
+                          petitions = user_petitions_list)
+
+# маршрут для получения списка петиций в городе
+@router.post("/get_city_petitions")
+async def get_city_petitions(city: City):
+    try:
+        result = await send_to_get_data(PETITION_SERVICE_ADDRESS + '/get_city_petitions', city)
+        petitions = result[0]["petitions"]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return PetitionsCity(petitions = petitions), status.HTTP_200_OK
+    
+        
+# маршрут для получения полной информации по петиции
+@router.post("/get_petition_data")
+async def get_petition_data(petiton: PetitonID):
+    try:
+        result = await send_to_get_data(PETITION_SERVICE_ADDRESS + '/get_petition_data', petiton)
+        data = result[0]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return PetitionData(id = data["id"], 
+                        header = data["header"], 
+                        is_initiative = data["is_initiative"], 
+                        category = data["category"],
+                        description = data["description"],
+                        status = data["status"],
+                        petitioner_id = data["petitioner_id"],
+                        submission_time = data["submission_time"],
+                        latitude = data["latitude"],
+                        longitude = data["longitude"],
+                        region = data["region"],
+                        city_name = data["city_name"],
+                        likes_count = data["likes_count"]), status.HTTP_200_OK
